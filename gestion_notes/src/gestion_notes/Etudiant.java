631b85fb1d04a2bbe59e9c011335372067cea4d7
khairat allah <khairat allah@192.168.56.1>
2022-04-15 15:35:53 +0000

    implémentation de l'intérface comparable

--- gestion_notes/src/gestion_notes/Etudiant.java
+++ gestion_notes/src/gestion_notes/Etudiant.java
@@ -2,7 +2,7 @@ package gestion_notes;
 
 
 
-public class Etudiant {
+public class Etudiant implements Comparable<Etudiant> {
 	private int id;
 	private String nom;
 	private double note;
@@ -12,6 +12,12 @@ public class Etudiant {
 		this.id=id;
 		this.nom=nom;
 		this.note=10;
+	}
+	
+	public Etudiant( int id , String nom ,double note) {
+		this.id=id;
+		this.nom=nom;
+		this.note=note;
 	}
 	
 	//redéfinition de la méthode tostring 
@@ -26,6 +32,27 @@ public class Etudiant {
 		}
 		return (((Etudiant)obj).id==this.id);
 	}
+	
+	//l'implémentation de l interface comparable
+	@Override
+	public int compareTo(Etudiant e) {
+		if(this.note>=e.note) return 1;
+		if(this.note<e.note) return 0;
+		return -1;
+		
+		
+	}
+	
+	public static void main (String[] args) {
+		Etudiant e1 , e2 ,e3 ;
+		e1=new Etudiant(111,"ahmad");
+		e2=new Etudiant(222, "salma" , 12.00);
+		e3=new Etudiant(333, "fatima");
+		System.out.println(e1);
+		System.out.println(e2);
+		System.out.println(e3);
+	}
+	
 	
 
 }
