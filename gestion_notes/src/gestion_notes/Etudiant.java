9fdefc9a539c9c2d16f6eed8b24351f1348e4039
khairat allah <khairat allah@192.168.56.1>
2022-04-15 14:40:30 +0000

    declaration du constructeur

--- gestion_notes/src/gestion_notes/Etudiant.java
+++ gestion_notes/src/gestion_notes/Etudiant.java
@@ -4,5 +4,12 @@ public class Etudiant {
 	private int id;
 	private String nom;
 	private double note;
+	
+	/*creation du constructeur */
+	public Etudiant( int id , String nom) {
+		this.id=id;
+		this.nom=nom;
+		this.note=10;
+	}
 
 }
