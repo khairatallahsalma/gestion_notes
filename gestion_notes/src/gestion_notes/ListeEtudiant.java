6e67e944d87d8c6d9bfd132d5d3b6aaa791c7da4
khairat allah <khairat allah@192.168.56.1>
2022-04-15 15:45:37 +0000

    la redéfinition  de la méthode add

--- gestion_notes/src/gestion_notes/ListeEtudiant.java
+++ gestion_notes/src/gestion_notes/ListeEtudiant.java
@@ -2,6 +2,9 @@ package gestion_notes;
 import java.util.ArrayList;
 
 public class ListeEtudiant extends ArrayList<Etudiant> {
-	
+	public boolean add(Etudiant e) {
+		if(this.contains(e)) return false;
+		return super.add(e);
+	}
 
 }
