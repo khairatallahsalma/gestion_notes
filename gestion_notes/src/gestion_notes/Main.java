270bcb071cda5f744c28aa3d0b1f1b507e2a014a
khairat allah <khairat allah@192.168.56.1>
2022-04-15 16:43:43 +0000

    la creation des etudiants

--- gestion_notes/src/gestion_notes/Main.java
+++ gestion_notes/src/gestion_notes/Main.java
@@ -4,6 +4,10 @@ package gestion_notes;
 
 public class Main {
 	public static void main() {
+		Etudiant e1 , e2 , e3;
+		e1=new Etudiant(1, "hamid");
+		e2=new Etudiant(2, "driss");
+		e3=new Etudiant(1, "youssef");
 		
 		
 	}
